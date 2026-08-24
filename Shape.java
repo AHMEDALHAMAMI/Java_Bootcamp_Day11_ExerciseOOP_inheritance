65c75940a8101025b0d0dd290e6b6446b97108e5
AHMEDALHAMAMI <146365804+AHMEDALHAMAMI@users.noreply.github.com>
2026-08-24 12:50:11 +0100

    Implement Shape class with color and filled properties

---
+++ Shape.java
@@ -0,0 +1,35 @@
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape(){
+
+    }
+    public Shape(String color, boolean filled){
+        this.color=color;
+        this.filled=filled;
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public void setFilled(boolean filled){
+        this.filled=filled;
+    }
+
+    public boolean isFilled(){
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
